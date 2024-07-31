5b77c7ba5f7938709c2faba0ec53e08bad6be0fe
David Such <dsuch@reefwing.com.au>
2024-07-31 18:30:20 +1000

    Update eda_standardisation.py

--- src/General AI/eda_standardisation.py
+++ src/General AI/eda_standardisation.py
@@ -3,9 +3,12 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import os
+import os, time
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+
+# Start the timer for the complete script
+start_time = time.time()
 
 # Load the training data
 data_folder = os.path.expanduser("~/Documents/GitHub/AI-Advances/articles/exploratory_data_analysis/Preprocessed")
@@ -16,15 +19,20 @@ data = pd.read_csv(file_path)
 # Initialize the StandardScaler
 scaler = StandardScaler()
 
-# Select the columns to standardize (excluding non-numeric columns)
-columns_to_standardize = data.select_dtypes(include=[float, int]).columns
+# Select the columns to standardise (excluding non-numeric columns)
+columns_to_standardise = data.select_dtypes(include=[float, int]).columns
 
 # Standardize the selected columns
-data[columns_to_standardize] = scaler.fit_transform(data[columns_to_standardize])
+data[columns_to_standardise] = scaler.fit_transform(data[columns_to_standardise])
 
-# Save the standardized data to a new CSV file
-new_file_name = 'standardized_training_data.csv'
+# Save the standardised data to a new CSV file
+new_file_name = 'standardised_training_data.csv'
 new_file_path = os.path.join(data_folder, new_file_name)
 data.to_csv(new_file_path, index=False)
 
-print(f"Standardized data saved to {new_file_path}")
+print(f"Standardised data saved to {new_file_path}")
+
+# Print the elapsed time for standardisation
+total_elapsed_time = time.time() - start_time
+total_minutes, total_seconds = divmod(total_elapsed_time, 60)
+print(f"Total script execution time: {int(total_minutes)} minutes and {total_seconds:.2f} seconds")
